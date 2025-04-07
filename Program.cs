d11cad8a73228b178f7e551e343dabd8a1498790
MarcelWiesko <marcel.wiesko@icloud.com>
2025-04-07 14:07:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _95_interfejsy_Poziom_Trudnosci
+{
+    internal class Program
+    {
+        enum PoziomTrudnosci
+        {
+            Łatwy, Średni, Trudny
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj poziom trudnosci");
+            string wybor = Console.ReadLine();
+            Console.WriteLine($"Wybrałeś {wybor} poziom trudnosci");
+        }
+    }
+    
+
+}
